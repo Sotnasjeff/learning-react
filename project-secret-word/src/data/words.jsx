987a5fba9a3ffb429608e22eaf7ebd130ed5b93b
Jefferson Santos <jeffdeutsch1@gmail.com>
2025-02-04 01:06:31 -0300

    feat: created project secret wored

---
+++ project-secret-word/src/data/words.jsx
@@ -0,0 +1,8 @@
+export const wordList = {
+    carro: ["Ferrari", "Mercedes", "Renault", "Lamborghini", "Chevrolet"],
+    formula1: ["Ferrari", "Kicksauber", "Redbull", "Mercedes","Mclaren", "VisaCashRedBull","Haas","Alpine","Williams","AstonMartin"],
+    corpo: ["Braço", "Perna", "Orelha", "Pulmão", "Coração", "Olhos", "Cabelo", "Boca"],
+    programação: ["Golang", "React", "Java", "Kotlin", "Javascript", "Typescript", "Python", "Dart", "Rust", "Clojure"],
+    fruta: ["Banana", "Maçã", "Laranja", "Pera", "Lichia", "Mamão", "Morango", "Caju", "Jaboticaba", "Limão"],
+    piloto: ["MaxVerstappen","LewisHamilton","EstebanOcon", "NicoHulkenberg","OscarPiastri","FernandoAlonso","YukiTsunoda","GabrielBortoleto","LiamLawson","IsackHadjar","AndreaKimiAntonelli","GeorgeRussel","LandoNorris","CharlesLeclerc","OliverBearman","LanceStroll","JackDoohan","PierreGasly","AlexanderAlbon","CarlosSainz"]
+};
